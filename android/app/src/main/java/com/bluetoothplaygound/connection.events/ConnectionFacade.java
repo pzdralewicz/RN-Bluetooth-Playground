4c7495840e9b6899ce138a2a3061b2a06aac1eb0
Paweł Zdralewicz <pawel.zdralewicz@gmail.com>
2020-06-14 15:39:22 +0200

    Migrate VisaMobile solution

---
+++ android/app/src/main/java/com/bluetoothplaygound/connection.events/ConnectionFacade.java
@@ -0,0 +1,33 @@
+package com.visamobile.connection.events;
+
+import android.bluetooth.BluetoothDevice;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableMap;
+
+public class ConnectionFacade {
+
+  private final ConnectionDispatcher connectionDispatcher = new ConnectionDispatcher();
+
+  private WritableMap queryWritableMapFromDevice(BluetoothDevice device) {
+    WritableMap params = Arguments.createMap();
+    params.putString("deviceName", device.getName());
+    params.putString("deviceAddress", device.getAddress());
+    return params;
+  }
+
+  public void connected(BluetoothDevice device, ReactApplicationContext reactContext) {
+    connectionDispatcher.sendEvent(
+        reactContext, STATUS.CONNECTED.name(), queryWritableMapFromDevice(device));
+  }
+
+  public void disconnected(BluetoothDevice device, ReactApplicationContext reactContext) {
+    connectionDispatcher.sendEvent(
+        reactContext, STATUS.DISCONNECTED.name(), queryWritableMapFromDevice(device));
+  }
+
+  private enum STATUS {
+    CONNECTED,
+    DISCONNECTED
+  }
+}
---
+++ android/app/src/main/java/com/bluetoothplaygound/connection.events/ConnectionListener.java
@@ -0,0 +1,34 @@
+package com.visamobile.connection.events;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import com.facebook.react.bridge.ReactApplicationContext;
+
+public class ConnectionListener {
+
+  private final ReactApplicationContext reactContext;
+
+  public final BroadcastReceiver mReceiver =
+      new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+          String action = intent.getAction();
+          BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+          ConnectionFacade connectionFacade = new ConnectionFacade();
+          if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+            Log.i("BluetoothListener", "Połączono urządzenie: " + device.getName());
+            connectionFacade.connected(device, reactContext);
+          } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+            Log.i("BluetoothListener", "Rozłączono urządzenie: " + device.getName());
+            connectionFacade.disconnected(device, reactContext);
+          }
+        }
+      };
+
+  public ConnectionListener(ReactApplicationContext reactContext) {
+    this.reactContext = reactContext;
+  }
+}
